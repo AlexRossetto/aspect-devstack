b9d7c7a6e872242b771ce258b308c73efda6179a
Alexandre Rossetto <drossetto.alexandre@gmail.com>
2024-10-28 23:57:17 -0300

    removing node modules and build folder

--- backend/dist/src/routes/middleware/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = middleware;
-function middleware(_, HttpResponse, NextFunction) {
-    HttpResponse.setHeader('X-C-Status', status || 'false');
-    NextFunction();
-}
